cacc039633acb5fcb25b57f7bd10267d5741b68f
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-08-16 03:23:40 +0600

    add zsh plugin repos

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -25,4 +25,12 @@ fi
 #   sudo ln -sfv /bin/zsh /private/var/select/sh
 # fi
 
+
 defaults write -g ApplePressAndHoldEnabled -bool false 
+
+# zsh plugins
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_PATH/zsh-vi-mode
+git clone https://github.com/romkatv/powerlevel10k $ZSH_PATH/powerlevel10k
+git clone https://github.com/rupa/z $ZSH_PATH/z
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH_PATH/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting $ZSH_PATH/zsh-syntax-highlighting

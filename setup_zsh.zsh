6f059d466418486b17e2650962ca2d4e43225af2
FahadAminShovon <29832989+FahadAminShovon@users.noreply.github.com>
2022-08-16 04:35:38 +0600

    don't upgrade shell

--- setup_zsh.zsh
+++ setup_zsh.zsh
@@ -4,19 +4,19 @@ echo "\n <<< Starting ZSH Setup >>> \n"
 
 # Installation unnecessary; it's in the Brewfile.
 
-if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
-  echo '/opt/homebrew/bin/zsh already exits in /etc/shells'
-else
-  echo "Enter superuser (sudo) password to edit /etc/shells"
-  echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
-fi
+# if grep -Fxq '/opt/homebrew/bin/zsh' '/etc/shells'; then
+#   echo '/opt/homebrew/bin/zsh already exits in /etc/shells'
+# else
+#   echo "Enter superuser (sudo) password to edit /etc/shells"
+#   echo '/opt/homebrew/bin/zsh' | sudo tee -a '/etc/shells' >/dev/null
+# fi
 
-if [ "$SHELL" = '/opt/homebrew/bin/zsh' ] ; then
-  echo '$SHELL is already' $SHELL
-else
-  echo "Enter user password to change login shell"
-  chsh -s '/opt/homebrew/bin/zsh'
-fi
+# if [ "$SHELL" = '/opt/homebrew/bin/zsh' ] ; then
+#   echo '$SHELL is already' $SHELL
+# else
+#   echo "Enter user password to change login shell"
+#   chsh -s '/opt/homebrew/bin/zsh'
+# fi
 
 # if sh --version | grep -q zsh; then
 #   echo '/private/var/selected/sh already linked to /bin/zsh'
